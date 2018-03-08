04b31b58d1f5c461bc126b64420aea714917c642
unknown <tiphaine.brunet@numericable.fr>
2018-03-08 16:48:14 +0100

    projetandroid

---
+++ app/src/main/java/com/brunet/tipha/applicationessai/MainActivity.java
@@ -0,0 +1,13 @@
+package com.brunet.tipha.applicationessai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
